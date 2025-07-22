83272f6249f9f23e7d2376c00d903a68ac06248a
star <natepy19@gmail.com>
2025-07-22 18:48:08 +0700

    first commit

---
+++ 8kyu/squareSum/squareSum.js
@@ -0,0 +1,8 @@
+function squareSum(numbers){
+  let sum = 0;
+  for(let i = 0; i<numbers.length; i++){
+    sum += numbers[i] * numbers[i];
+  }
+  return sum;
+}
+console.log(squareSum([1, 2, 2]));
