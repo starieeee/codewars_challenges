779ab056f1d141a367bb53f6887aef0579f87400
star <natepy19@gmail.com>
2025-08-14 19:26:10 +0700

    commit

--- correctMistake.js
+++ correctMistake.js
@@ -17,3 +17,10 @@ function correct(string) {
 console.log(correct("L0ND0N"));     // LONDON
 console.log(correct("DUBL1N"));     // DUBLIN
 console.log(correct("51NGAP0RE"));  // SINGAPORE
+
+// Second method
+
+function correctString(str){
+    return str.replace(/5/g, "S").replace(/0/g, "O").replace(/1/g, "I");
+}
+console.log(correctString("L0ND0N"));     
