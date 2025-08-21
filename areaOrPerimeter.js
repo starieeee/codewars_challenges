51e4ea8b25ebb731b970a4bf8b79f929a698d6bb
star <natepy19@gmail.com>
2025-08-21 10:55:37 +0700

    commit

--- areaOrPerimeter.js
+++ areaOrPerimeter.js
@@ -9,3 +9,10 @@ const areaOrPerimeter = function(l , w) {
 };
 console.log(areaOrPerimeter(3,3));
 console.log(areaOrPerimeter(4,3));
+
+// Second method
+function areaOrrPeri(length, width){
+    return length === width ? length * width : 2 * (length + width);
+}
+console.log(areaOrrPeri(3,3));
+console.log(areaOrrPeri(4,3));
