2722bd4f3d9fb839e08f22b383e86a06ffb52018
star <natepy19@gmail.com>
2025-08-26 22:55:27 -0500

    commit

---
+++ goals.js
@@ -0,0 +1,5 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+console.log(goals(10, 5, 2));
