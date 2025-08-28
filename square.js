42dc63c06faf6da4f274d2dcd82167e5f98552f4
star <natepy19@gmail.com>
2025-08-28 09:20:30 -0500

    commit

--- square.js
+++ square.js
@@ -10,3 +10,9 @@ function squareTwo(a,b){
     return a ** b;
 }
 console.log(squareTwo(3,2));
+
+// Third method
+function squareThree(a,b){
+    return Math.pow(a, b);
+}
+console.log(squareThree(3,2));
