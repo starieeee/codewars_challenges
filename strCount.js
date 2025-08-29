92365b65c9936e484e1ac6df071b5b81f9ca1595
star <natepy19@gmail.com>
2025-08-29 09:29:15 -0500

    commit

---
+++ strCount.js
@@ -0,0 +1,11 @@
+function strCount(str, letter){  
+  //code here
+  let count = 0;
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === letter){
+      count ++;
+    }
+  }
+  return count;
+}
+console.log(strCount("Hello", 'o'));
